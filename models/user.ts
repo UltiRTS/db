5113eaff2907fb934d9cf46a25b1ffcff21a4837
Chi Zhang <zhangchi0104@live.com>
2023-04-24 11:11:44 +0800

    feat: Modeled User Inventory

--- models/user.ts
+++ models/user.ts
@@ -1,93 +1,133 @@
-import { Entity, PrimaryGeneratedColumn, Column, OneToMany, ManyToMany, JoinTable, ManyToOne} from "typeorm"
-import { Confirmation } from "./confirmation"
-import { Chat } from "./chat"
-import * as crypto from 'crypto'
-import { Adventure } from "./adventure"
+/** @format */
+
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  OneToMany,
+  ManyToMany,
+  JoinTable,
+  ManyToOne,
+} from 'typeorm';
+import { Confirmation } from './confirmation';
+import { Chat } from './chat';
+import * as crypto from 'crypto';
+import { Adventure } from './adventure';
 // import c from "crypto"
 
 @Entity()
 export class User {
-    @PrimaryGeneratedColumn()
-    id: number
+  @PrimaryGeneratedColumn()
+  id: number;
 
-    @Column('varchar', {unique: true})
-    username: string
+  @Column('varchar', { unique: true })
+  username: string;
 
-    @Column()
-    hash: string
+  @Column()
+  hash: string;
 
-    @Column()
-    salt: string
+  @Column()
+  salt: string;
 
-    @Column('int', {default: 0})
-    accessLevel: number
+  @Column('int', { default: 0 })
+  accessLevel: number;
 
-    @Column('int', {default: 0})
-    exp: number
+  @Column('int', { default: 0 })
+  exp: number;
 
-    @Column('int', {default: 0})
-    sanity: number
+  @Column('boolean', { default: false })
+  blocked: boolean;
 
-    @Column('boolean', {default: false})
-    blocked: boolean
+  @Column('int', { default: 0 })
+  winCount: number;
 
-    @Column('int', {default: 0})
-    winCount: number
+  @Column('int', { default: 0 })
+  loseCount: number;
 
-    @Column('int', {default: 0})
-    loseCount: number
+  @OneToMany(() => Confirmation, (confirmation) => confirmation.user)
+  confirmations: Confirmation[];
 
-    @OneToMany(() => Confirmation, (confirmation) => confirmation.user)
-    confirmations: Confirmation[]
+  @OneToMany(() => Chat, (chat) => chat.author)
+  chats: Chat[];
 
-    @OneToMany(() => Chat, (chat) => chat.author)
-    chats: Chat[]
+  @ManyToMany(() => Adventure)
+  @JoinTable()
+  adventures: Adventure[];
 
-    @ManyToMany(() => Adventure)
-    @JoinTable()
-    adventures: Adventure[]
+  @ManyToMany(() => User, (user) => user.id)
+  @JoinTable()
+  friends: User[];
 
-    @ManyToMany(() => User, (user) => user.id)
-    @JoinTable()
-    friends: User[]
+  @OneToMany(() => Mark, (mark) => mark.user)
+  marks: Mark[];
 
-    @OneToMany(() => Mark, (mark) => mark.user)
-    marks: Mark[]
+  @OneToMany(() => Mark, (mark) => mark.target)
+  reverseMarks: Mark[];
 
-    @OneToMany(() => Mark, (mark) => mark.target)
-    reverseMarks: Mark[]
+  @OneToMany(() => Inventory, (inventory) => inventory.user)
+  inventory: Inventory[];
 
-    static saltNhash(password: string) {
-        const salt = crypto.randomBytes(16).toString('hex'); 
-    
-        // Hashing user's salt and password with 1000 iterations, 
-        
-        const hash = crypto.pbkdf2Sync(password, salt,  
-        1000, 64, `sha512`).toString(`hex`); 
+  static saltNhash(password: string) {
+    const salt = crypto.randomBytes(16).toString('hex');
 
-        return {
-            salt, hash
-        }
-    }
+    // Hashing user's salt and password with 1000 iterations,
 
-    verify(password: string) {
-        var hash = crypto.pbkdf2Sync(password,  
-            this.salt, 1000, 64, `sha512`).toString(`hex`); 
-            return this.hash === hash; 
-    }
+    const hash = crypto
+      .pbkdf2Sync(password, salt, 1000, 64, `sha512`)
+      .toString(`hex`);
+
+    return {
+      salt,
+      hash,
+    };
+  }
+
+  verify(password: string) {
+    var hash = crypto
+      .pbkdf2Sync(password, this.salt, 1000, 64, `sha512`)
+      .toString(`hex`);
+    return this.hash === hash;
+  }
 }
 
 @Entity()
 export class Mark {
-    @PrimaryGeneratedColumn()
-    id: number
+  @PrimaryGeneratedColumn()
+  id: number;
 
-    @Column()
-    mark: string
+  @Column()
+  mark: string;
 
-    @ManyToOne(() => User, (user) => user.marks)
-    user: User
+  @ManyToOne(() => User, (user) => user.marks)
+  user: User;
 
-    @ManyToOne(() => User, (user) => user.reverseMarks)
-    target: User
+  @ManyToOne(() => User, (user) => user.reverseMarks)
+  target: User;
+}
+
+@Entity()
+export class Inventory {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string; // name of the item
+
+  @Column('int', { default: 0 })
+  qty: number; // quantity of the item
+
+  @Column()
+  type: string; // type of the item
+
+  @Column()
+  scope: string; // scope of the item
+
+  @Column()
+  alias: string; // alias of the item
+
+  @Column()
+  description: string; // description of the item
+
+  @ManyToOne(() => User, (user) => user.inventory)
+  user: User;
 }
